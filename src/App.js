f5adf8de26620328fbb8face7d0885c992cb9a15
loophalen <abharris@outlook.com>
2021-10-31 14:38:07 -0400

    inital commit with navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Austin Harris",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
+      ],
+      home: {
+        title: "Code Austin Code",
+        subTitle: "Austin is learning to code React",
+        text: "Projects below",
+      },
+      about: {
+        title: "About",
+      },
+      contact: {
+        title: "Contact Me",
+      },
+    };
+  }
+
   render() {
-    return <div>Code Austin Code</div>;
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Austin Harris</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ms-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/">
+                  About
+                </Link>
+                <Link className="nav-link" to="/">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    );
   }
 }
 
