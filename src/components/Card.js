4da1d9406bc341e5e26d35f400b706f077c7d047
loophalen <abharris@outlook.com>
2021-11-01 17:54:58 -0400

    Added React Spring and intial styling for Cards in Carousel Component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import CardInfo from "../components/CardInfo";
+
 function Card(props) {
   return (
-    <div>
-      <img className="" src={props.item.imgSrc} alt={props.item.imgSrc} />
+    <div
+      className="d-inline-block a-card"
+      onClick={(e) => props.click(props.item)}
+    >
+      <img
+        className="a-card-image"
+        src={props.item.imgSrc}
+        alt={props.item.imgSrc}
+      />
+      {props.item.selected && (
+        <CardInfo
+          title={props.item.title}
+          subTitle={props.item.subTitle}
+          link={props.item.Link}
+        />
+      )}
     </div>
   );
 }
