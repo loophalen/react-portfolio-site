6d1eba38297086164687c6f0a9746b536a496e3a
loophalen <abharris@outlook.com>
2021-11-02 12:53:17 -0400

    added content component and about page with hero component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Content from "../components/Content";
 
 function AboutPage(props) {
-  return <p>AboutPage Works!</p>;
+  return (
+    <div>
+      <Hero title={props.title} />
+      <Content>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Content>
+    </div>
+  );
 }
 
 export default AboutPage;
